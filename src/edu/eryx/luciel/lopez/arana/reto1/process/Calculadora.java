58d4cdd1718db828f3d4f0e051f700c29d4c8ef0
Eryx-Lopez <122893188+Eryx-Lopez@users.noreply.github.com>
2023-01-26 11:46:06 -0600

    Calculadora.suma

---
+++ src/edu/eryx/luciel/lopez/arana/reto1/process/Calculadora.java
@@ -0,0 +1,7 @@
+package edu.eryx.luciel.lopez.arana.reto1.process;
+
+public class Calculadora {
+    public static int sumarEnteros (int numero1, int numero2){
+        return numero1 + numero1;
+    }
+}
