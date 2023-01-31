02624778aee206cd63e85a2e4f5e64a5d9df20f2
Eryx-Lopez <122893188+Eryx-Lopez@users.noreply.github.com>
2023-01-31 08:53:19 -0600

    Update Calculadora.java

--- src/edu/eryx/luciel/lopez/arana/reto1/process/Calculadora.java
+++ src/edu/eryx/luciel/lopez/arana/reto1/process/Calculadora.java
@@ -2,7 +2,7 @@ package edu.eryx.luciel.lopez.arana.reto1.process;
 
 public class Calculadora {
     public static int sumarEnteros (int numero1, int numero2){
-        return numero1 + numero1;
+        return numero1 + numero2;
     }
     public static int restarEnteros (int numero1, int numero2){
         return numero1 - numero2;
