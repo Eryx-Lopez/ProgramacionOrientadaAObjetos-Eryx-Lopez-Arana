ac4d36717a753378dc225161cf4768a16c604243
Eryx-Lopez <122893188+Eryx-Lopez@users.noreply.github.com>
2023-02-02 09:59:20 -0600

    Update Piramide.java

--- src/edu/eryx/luciel/lopez/arana/reto2/piramide/process/Piramide.java
+++ src/edu/eryx/luciel/lopez/arana/reto2/piramide/process/Piramide.java
@@ -5,18 +5,12 @@ import java.util.Scanner;
 public class Piramide {
     /**
      * Clase para pedir e imprimir el número de asteriscos que se da por el usuario
-     * @param
+     *
      * @author eryxl
      * */
 
-    public static void main(String[] args) {
 
-    }
-
-
-    public static void imprimirAsteriscos(){
-        Scanner asteriscos = new Scanner(System.in);
-        int numAsteriscos = asteriscos.nextInt();
+    public static void imprimirAsteriscos(int numAsteriscos){
 
         for (int i=0; i<numAsteriscos; i++){ //número de filas de asteriscos
             for (int j=1; j<numAsteriscos-i; j++){
@@ -29,5 +23,6 @@ public class Piramide {
         }
     }
 
-
+    //https://www.lawebdelprogramador.com/codigo/Java/6185-Piramide-de-asteriscos-en-Java.html
+    //https://www.youtube.com/watch?v=ilBg5qHMMzU
 }
