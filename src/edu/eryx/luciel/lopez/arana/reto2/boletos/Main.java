2924eaf8f5e9c1f7a43968c8c77db434da71872c
Eryx-Lopez <122893188+Eryx-Lopez@users.noreply.github.com>
2023-02-02 15:55:06 -0600

    Update Main.java

--- src/edu/eryx/luciel/lopez/arana/reto2/boletos/Main.java
+++ src/edu/eryx/luciel/lopez/arana/reto2/boletos/Main.java
@@ -1,4 +1,9 @@
 package edu.eryx.luciel.lopez.arana.reto2.boletos;
 
+import edu.eryx.luciel.lopez.arana.reto2.boletos.ui.CLI;
+
 public class Main {
+    public static void main(String[] args) {
+    CLI.showMenu();
+    }
 }
