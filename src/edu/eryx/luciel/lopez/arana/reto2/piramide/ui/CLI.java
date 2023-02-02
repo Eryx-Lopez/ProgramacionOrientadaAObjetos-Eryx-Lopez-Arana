a93f48eeb48ad98cc39a25379d7bee3d2b61702b
Eryx-Lopez <122893188+Eryx-Lopez@users.noreply.github.com>
2023-02-02 09:59:12 -0600

    Update CLI.java

--- src/edu/eryx/luciel/lopez/arana/reto2/piramide/ui/CLI.java
+++ src/edu/eryx/luciel/lopez/arana/reto2/piramide/ui/CLI.java
@@ -2,11 +2,15 @@ package edu.eryx.luciel.lopez.arana.reto2.piramide.ui;
 
 import edu.eryx.luciel.lopez.arana.reto2.piramide.process.Piramide;
 
+import java.util.Scanner;
+
 public class CLI {
 
     public static void pedirAsteriscos(){
         System.out.println("Inserte el número del que desea su triángulo de asteriscos:");
-        Piramide.imprimirAsteriscos();
+        Scanner numAsterisco = new Scanner(System.in);
+        int asteriscos = numAsterisco.nextInt();
+        Piramide.imprimirAsteriscos(asteriscos);
         }
     }
 
