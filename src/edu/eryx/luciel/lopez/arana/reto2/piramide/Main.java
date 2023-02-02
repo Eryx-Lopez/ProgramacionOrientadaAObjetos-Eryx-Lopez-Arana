583f7c8df7f66d9b5bfec6f0d4a8577fdafca1dc
Eryx-Lopez <122893188+Eryx-Lopez@users.noreply.github.com>
2023-02-02 17:51:07 -0600

    Update Main.java

--- src/edu/eryx/luciel/lopez/arana/reto2/piramide/Main.java
+++ src/edu/eryx/luciel/lopez/arana/reto2/piramide/Main.java
@@ -1,5 +1,7 @@
 /**
  * Corre la java class CLI, la cual a su vez corre la java class Piramide, lo que ejecuta el programa completo
+ *
+ * @author eryxl
  * */
 
 package edu.eryx.luciel.lopez.arana.reto2.piramide;
