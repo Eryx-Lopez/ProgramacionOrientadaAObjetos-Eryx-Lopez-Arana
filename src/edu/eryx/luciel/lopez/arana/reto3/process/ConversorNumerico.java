5a9ca2e0232cf4dde9842b47cb8124d6833f33c3
Eryx-Lopez <122893188+Eryx-Lopez@users.noreply.github.com>
2023-02-07 09:24:41 -0600

    Create ConversorNumerico.java
    Clase de conversor numérico
    Decimal a binario

---
+++ src/edu/eryx/luciel/lopez/arana/reto3/process/ConversorNumerico.java
@@ -0,0 +1,16 @@
+package edu.eryx.luciel.lopez.arana.reto3.process;
+
+/**
+ * Esta clase convierte
+ */
+public class ConversorNumerico {
+    /** este método recibe un string en base decimal y regresa la conversión de ese número
+     * en formato binario.
+     *
+     * @param decimal
+     * @return el número en formato binario
+     */
+    public static String convertirABinario(String decimal){
+
+    }
+}
