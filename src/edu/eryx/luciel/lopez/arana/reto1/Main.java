812dcd2f65e23a1bca6d760679e15708405eac12
Eryx-Lopez <122893188+Eryx-Lopez@users.noreply.github.com>
2023-02-14 09:27:59 -0600

    -Reto1

--- src/edu/eryx/luciel/lopez/arana/reto1/Main.java
+++
@@ -1,9 +0,0 @@
-package edu.eryx.luciel.lopez.arana.reto1;
-
-import edu.eryx.luciel.lopez.arana.reto1.ui.CLI;
-
-public class Main {
-    public static void main(String[] args) {
-     CLI.mostrarMenu();
-    }
-}
