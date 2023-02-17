f956d81d00ee0b6bd1d85904e14934c50e742aac
Eryx-Lopez <122893188+Eryx-Lopez@users.noreply.github.com>
2023-02-17 12:34:13 -0600

    UpdateViernes

--- src/edu/eryx/luciel/lopez/arana/reto4/process/InformeDeVentas.java
+++ src/edu/eryx/luciel/lopez/arana/reto4/process/InformeDeVentas.java
@@ -1,10 +1,12 @@
 package edu.eryx.luciel.lopez.arana.reto4.process;
 
-import java.util.ArrayList;
-import java.util.List;
 
+/**
+ *
+ *
+ */
 public class InformeDeVentas {
-    double [][] tablaDeVentas= {
+    double [][] tablaDeVentas = {
             {111, 483, 471, 427},
             {192, 500, 355, 158},
             {289, 470, 474, 160},
@@ -13,21 +15,48 @@ public class InformeDeVentas {
 
     /**
      *
-     * @param tabla
+     * @param tabla es un areglo que contiene el número de ventas según cada trimestre
      * @return el valor más grande de la
      */
-    public static double getMaxProductFromTrimester(double[][] tabla){
-        double max = Double.MAX_VALUE; //El valor más pequeño existente (-infinito)
-       // int auxIndex = 0;
+    public static double getMaxProductFromTrimester(double[] tabla){
+        double maxChocolate = Double.MAX_VALUE; //El valor más pequeño existente (-infinito)
+        double maxVainilla = Double.MAX_VALUE;
+        double maxFresa = Double.MAX_VALUE;
+        double maxOreo = Double.MAX_VALUE;
 
-        for (int i = 0; i < tabla[0].length; i++){ //i representa las filas y j las columnas
-            for (int j = 0; j < tabla.length; j++){
-                if  ( tabla[i][j]>max){
-                    max = tabla[i][j];
+        for (int i = 0; i < tabla[0]; i++){ //i representa las filas y
+            for (int j = 0; j < tabla.length; j++){ //j las columnas
+                if  ( tabla[i]>maxChocolate){
+                    maxChocolate = tabla[i];
                 }
+
             }
         }
-        return max;
-        ayuda no sé que estoy haciendo
+        for (int i = 0; i < tabla[1]; i++){ //i representa las filas y
+            for (int j = 0; j < tabla.length; j++){ //j las columnas
+                if  ( tabla[i]>maxVainilla){
+                    maxVainilla= tabla[i];
+                }
+
+            }
+        }
+        for (int i = 0; i < tabla[2]; i++){ //i representa las filas y
+            for (int j = 0; j < tabla.length; j++){ //j las columnas
+                if  ( tabla[i]>maxFresa){
+                    maxFresa= tabla[i];
+                }
+
+            }
+        }
+        for (int i = 0; i < tabla[3]; i++){ //i representa las filas y
+            for (int j = 0; j < tabla.length; j++){ //j las columnas
+                if  ( tabla[i]>maxOreo){
+                    maxOreo= tabla[i];
+                }
+
+            }
+        }
+
+        return maxOreo;
     }
 }
--- src/edu/eryx/luciel/lopez/arana/reto4/ui/CLI.java
+++ src/edu/eryx/luciel/lopez/arana/reto4/ui/CLI.java
@@ -1,4 +1,10 @@
 package edu.eryx.luciel.lopez.arana.reto4.ui;
 
+import edu.eryx.luciel.lopez.arana.reto4.process.InformeDeVentas;
+
 public class CLI {
+    public static void main(String[] args) {
+
+
+    }
 }
