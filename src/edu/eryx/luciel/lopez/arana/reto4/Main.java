d48b0c53249a7c475ce62b32a36b0c54bcdbb748
Eryx-Lopez <122893188+Eryx-Lopez@users.noreply.github.com>
2023-02-18 21:10:04 -0600

    Final

--- src/edu/eryx/luciel/lopez/arana/reto4/Main.java
+++ src/edu/eryx/luciel/lopez/arana/reto4/Main.java
@@ -1,8 +1,12 @@
 package edu.eryx.luciel.lopez.arana.reto4;
-
-import edu.eryx.luciel.lopez.arana.reto4.process.InformeDeVentas;
 import edu.eryx.luciel.lopez.arana.reto4.ui.CLI;
 
+/**
+ * Manda a llamar la clase CLI, que a su vez manda a llamar a la clase
+ * InformeDeVentas, lo que ejecuta el código entero
+ *
+ * @author eryxl
+ */
 public class Main {
     public static void main(String[] args) {
         CLI.showTabla();
--- src/edu/eryx/luciel/lopez/arana/reto4/process/InformeDeVentas.java
+++ src/edu/eryx/luciel/lopez/arana/reto4/process/InformeDeVentas.java
@@ -1,8 +1,17 @@
 package edu.eryx.luciel.lopez.arana.reto4.process;
-
 import java.util.ArrayList;
 
+/**
+ * Genera dos métodos que obtienen el producto que más se vende por sabor y por trimestre general en ventas
+ *
+ * @author eryxl
+ */
 public class InformeDeVentas {
+    /**
+     *Lee los valores de una tabla y regresa el valor máximo de cada fila en un array
+     * @param tabla la tabla que se va a leer
+     * @return los valores máximos
+     */
     public static ArrayList<Integer> getMaxProductFromTrimester(double[][] tabla) {
 
         ArrayList<Integer> maximos = new ArrayList<>();
@@ -15,17 +24,20 @@ public class InformeDeVentas {
                     auxIndex = j;
                     max = tabla[i][j];
                 }
-
             }
             maximos.add(auxIndex);
         }
-
         return maximos;
     }
 
+    /**
+     * Suma los valores en el arreglo y devuelve la posición y valor del más grande.
+     * @param tabla la tabla que se va a leer
+     * @return auxIndex que es la posición de la suma más grande de cada trimestre,
+     * el valor más grande de verntas generales
+     */
     public static int maxTrimester(double[][] tabla){
         int maxTrimester = 0;
-
         int auxIndex = 0;
 
         for (int i = 0; i < tabla.length; i++){
@@ -40,5 +52,6 @@ public class InformeDeVentas {
         }
         return auxIndex;
     }
+
 
 }
